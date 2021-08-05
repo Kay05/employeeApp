8366f85144a6fdf432f5490da3f5d475e3de0912
Kuda <kuda@tangentsolutions.co.za>
2021-08-05 18:10:30 +0200

    update frontend unit tests

--- frontend/src/views/EditEmployee.vue
+++ frontend/src/views/EditEmployee.vue
@@ -82,7 +82,6 @@ export default {
           this.$store.commit("setIsLoading", false);
         })
         .catch((error) => {
-          console.log(error);
           this.$store.commit("setIsLoading", false);
         });
     },
